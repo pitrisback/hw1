c1f64f9255c457053ef0c617aa4166239e37ed3d
Pitrisback <pmn.back@gmail.com>
2019-11-20 15:33:41 +0100

    Print more info from topic

--- src/april_parse.cpp
+++ src/april_parse.cpp
@@ -8,6 +8,13 @@ void chatterCallback(const apriltag_ros::AprilTagDetectionArray& msg)
     if (msg.detections.size() > 0) {
         apriltag_ros::AprilTagDetection first_detection = msg.detections[0];
         ROS_INFO("I heard: [%d]", first_detection.id[0]);
+        ROS_INFO("Position x: [%f]", first_detection.pose.pose.pose.position.x);
+        ROS_INFO("Position y: [%f]", first_detection.pose.pose.pose.position.y);
+        ROS_INFO("Position z: [%f]", first_detection.pose.pose.pose.position.z);
+        ROS_INFO("Orientation w: [%f]", first_detection.pose.pose.pose.orientation.w);
+        ROS_INFO("Orientation x: [%f]", first_detection.pose.pose.pose.orientation.x);
+        ROS_INFO("Orientation y: [%f]", first_detection.pose.pose.pose.orientation.y);
+        ROS_INFO("Orientation z: [%f]", first_detection.pose.pose.pose.orientation.z);
     } else {
         ROS_INFO("No tag found");
     }
